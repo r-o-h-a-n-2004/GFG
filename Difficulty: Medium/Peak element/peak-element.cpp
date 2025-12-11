1563145b1abc9058ffb6694ef37548937d044a35
r-o-h-a-n-2004 <134271385+r-o-h-a-n-2004@users.noreply.github.com>
2025-12-11 19:42:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Peak element/peak-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int peakElement(vector<int> &nums) {
+        int l = 0, r = nums.size() - 1;
+
+    while(l < r) {
+        int mid = (l + r) / 2;
+
+        if(nums[mid] < nums[mid + 1])
+            l = mid + 1;      // Peak on right
+        else
+            r = mid;          // Peak on left
+    }
+
+    return l;
+        
+    }
+};
